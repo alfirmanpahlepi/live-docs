58c0a5389c805b357912eefc8c7a9738164c5a7d
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2024-11-02 18:10:23 +0700

    feat: homepage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,8 @@
+import AddDocumentBtn from "@/components/AddDocumentBtn";
 import Header from "@/components/Header";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 const Home = async () => {
@@ -16,6 +18,33 @@ const Home = async () => {
           </SignedIn>
         </div>
       </Header>
+      {false ? (
+        <div className="document-list-container">
+          <div className="document-list-title">
+            <h3 className="text-28-semibold">All documents</h3>
+            {/* <AddDocumentBtn 
+              userId={clerkUser.id}
+              email={clerkUser.emailAddresses[0].emailAddress}
+            /> */}
+          </div>
+          <ul className="document-ul"></ul>
+        </div>
+      ) : (
+        <div className="document-list-empty">
+          <Image
+            src="/assets/icons/doc.svg"
+            alt="Document"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+
+          <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
+        </div>
+      )}
     </main>
   );
 };
