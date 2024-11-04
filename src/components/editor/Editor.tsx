d7e353855325ed423b3287d2bd91b68470a1c47c
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2024-11-05 04:48:24 +0700

    feat: floating comments

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,21 +1,27 @@
-'use client';
+"use client";
 
-import Theme from './plugins/Theme';
-import ToolbarPlugin from './plugins/ToolbarPlugin';
-import { HeadingNode } from '@lexical/rich-text';
-import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
-import { LexicalComposer } from '@lexical/react/LexicalComposer';
-import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
-import { ContentEditable } from '@lexical/react/LexicalContentEditable';
-import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
-import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
-import React from 'react';
+import Theme from "./plugins/Theme";
+import ToolbarPlugin from "./plugins/ToolbarPlugin";
+import { HeadingNode } from "@lexical/rich-text";
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import React from "react";
 
-import { FloatingComposer, FloatingThreads, liveblocksConfig, LiveblocksPlugin, useEditorStatus } from '@liveblocks/react-lexical'
-import Loader from '../Loader';
+import {
+  FloatingComposer,
+  FloatingThreads,
+  liveblocksConfig,
+  LiveblocksPlugin,
+  useEditorStatus,
+} from "@liveblocks/react-lexical";
+import Loader from "../Loader";
 
-import FloatingToolbarPlugin from './plugins/FloatingToolbarPlugin'
-import { useThreads } from '@liveblocks/react/suspense';
+import FloatingToolbarPlugin from "./plugins/FloatingToolbarPlugin";
+import { useThreads } from "@liveblocks/react/suspense";
 // import Comments from '../Comments';
 // import { DeleteModal } from '../DeleteModal';
 
@@ -27,19 +33,25 @@ function Placeholder() {
   return <div className="editor-placeholder">Enter some rich text...</div>;
 }
 
-export function Editor({ roomId, currentUserType }: { roomId: string, currentUserType: UserType }) {
+export function Editor({
+  roomId,
+  currentUserType,
+}: {
+  roomId: string;
+  currentUserType: UserType;
+}) {
   const status = useEditorStatus();
   const { threads } = useThreads();
 
   const initialConfig = liveblocksConfig({
-    namespace: 'Editor',
+    namespace: "Editor",
     nodes: [HeadingNode],
     onError: (error: Error) => {
       console.error(error);
       throw error;
     },
     theme: Theme,
-    editable: currentUserType === 'editor',
+    editable: currentUserType === "editor",
   });
 
   return (
@@ -51,7 +63,9 @@ export function Editor({ roomId, currentUserType }: { roomId: string, currentUse
         </div>
 
         <div className="editor-wrapper flex flex-col items-center justify-start">
-          {status === 'not-loaded' || status === 'loading' ? <Loader /> : (
+          {status === "not-loaded" || status === "loading" ? (
+            <Loader />
+          ) : (
             <div className="editor-inner min-h-[1100px] relative mb-5 h-fit w-full max-w-[800px] shadow-md lg:mb-10">
               <RichTextPlugin
                 contentEditable={
@@ -60,7 +74,7 @@ export function Editor({ roomId, currentUserType }: { roomId: string, currentUse
                 placeholder={<Placeholder />}
                 ErrorBoundary={LexicalErrorBoundary}
               />
-              {currentUserType === 'editor' && <FloatingToolbarPlugin />}
+              {currentUserType === "editor" && <FloatingToolbarPlugin />}
               <HistoryPlugin />
               <AutoFocusPlugin />
             </div>
